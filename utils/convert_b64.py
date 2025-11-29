400cb1e849696f74c51070933ab19c5b042ed8b3
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-11-28 19:54:18 -0500

    add image

--- utils/convert_b64.py
+++ utils/convert_b64.py
@@ -1,5 +1,7 @@
-import cv2
+import os
 import base64
+import cv2
+from datetime import datetime
 
 def frame_to_base64(frame):
     """
@@ -11,8 +13,8 @@ def frame_to_base64(frame):
     Returns:
         str: String base64 del frame comprimido
     """
-    # Resize a 320x180 (rectangular 16:9)
-    small = cv2.resize(frame, (320, 180))
+    # Resize a 
+    small = cv2.resize(frame, (1280, 720))
     
     # JPEG calidad 70 (muy liviano)
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 70]
@@ -22,3 +24,20 @@ def frame_to_base64(frame):
     jpg_b64 = base64.b64encode(buffer).decode("utf-8")
     
     return jpg_b64
+
+def save_b64_image(b64_str: str, alert_type: str, folder="img_alert"):
+    if b64_str is None:
+        return None
+
+    os.makedirs(folder, exist_ok=True)
+
+    ts = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"{alert_type}_{ts}.jpg"
+    path = os.path.join(folder, filename)
+
+    img_bytes = base64.b64decode(b64_str)
+
+    with open(path, "wb") as f:
+        f.write(img_bytes)
+
+    return path
